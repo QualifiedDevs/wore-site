4b57fade0da66d8dde447cef2eaa5dc268f77b1b
cryptominnow <minnow@spookysouls.com>
2022-02-05 22:24:45 -0600

    stateful stuff

--- src/components/PurchaseUI.tsx
+++ src/components/PurchaseUI.tsx
@@ -62,7 +62,7 @@ const PurchaseButton = styled(({ quantity, ...props }: any) => {
     <LoadingButton
       onClick={handleClick}
       loading={isLoading}
-      disabled={isMinting || quantity}
+      disabled={isMinting || (3 - mintedBalance) <= quantity }
       {...props}
     >
       Purchase {quantity}
