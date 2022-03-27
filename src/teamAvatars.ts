1bcd62a0c3d47e76ee02285cb885c4dd501a18fe
cryptominnow <minnow@spookysouls.com>
2022-03-27 17:43:15 -0500

    images

--- src/teamAvatars.ts
+++ src/teamAvatars.ts
@@ -1,13 +1,13 @@
-import rocky from "@public/team-avatars/rocky.png";
-import nathan from "@public/team-avatars/nathan.png";
-import david from "@public/team-avatars/david.png";
-import ezequiel from "@public/team-avatars/ezequiel.png";
-import declan from "@public/team-avatars/declan.png";
-import ruan from "@public/team-avatars/ruan.png";
-import dodge from "@public/team-avatars/dodge.png";
-import noah from "@public/team-avatars/noah.png";
-import ross from "@public/team-avatars/ross.png";
-import nina from "@public/team-avatars/nina.png";
+import rocky from "@public/team-avatars/rocky.jpg";
+import nathan from "@public/team-avatars/nathan.jpg";
+import david from "@public/team-avatars/david.jpg";
+import ezequiel from "@public/team-avatars/ezequiel.jpg";
+import declan from "@public/team-avatars/declan.jpg";
+import ruan from "@public/team-avatars/ruan.jpg";
+import dodge from "@public/team-avatars/dodge.jpg";
+import noah from "@public/team-avatars/noah.jpg";
+import ross from "@public/team-avatars/ross.jpg";
+import nina from "@public/team-avatars/nina.jpg";
 
 const avatars: {[key: string]: any} = { rocky, nathan, david, ezequiel, declan, ruan, dodge, noah, ross, nina }
 export default avatars;
